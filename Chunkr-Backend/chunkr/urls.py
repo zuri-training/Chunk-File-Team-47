a7069f98d179f0f780e74ab584f5a87d3786ced8
Aluko Ayomide <alukoayomide623@gmail.com>
2022-08-01 21:48:57 +0100

    Added icons on the frontend and few changes on the backend

--- Chunkr-Backend/chunkr/urls.py
+++ Chunkr-Backend/chunkr/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import views as user_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('register/', user_views.register, name='register'),
     path('chunkr/', include('chunkrio.urls')),
 ]
--- Chunkr-Backend/chunkrio/urls.py
+++ Chunkr-Backend/chunkrio/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "chunkrio"
+
 urlpatterns = [
     path('', views.home, name='chunkrio-home'),
     path('documentation/', views.documentation, name='chunkr-documentation'),
