544a6bd126f93351091b1b1abe6395689eab4dc0
Aluko Ayomide <alukoayomide623@gmail.com>
2022-08-05 17:17:41 +0100

    Created a user authentication system on the backend

--- Chunkr-Backend/chunkrio/models.py
+++ Chunkr-Backend/chunkrio/models.py
@@ -1,11 +1,21 @@
-from django.db import models
+# from django.db import models
 
 # Create your models here.
+# class File(models.Model):
+#     csv = models.FileField()
+#     json = models.FileField()
+
+
+# class Meta:
+#         abstract = True
+
+import uuid
+from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
 class File(models.Model):
-    csv = models.FileField()
-    json = models.FileField()
-
-
-class Meta:
-        abstract = True
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file_id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    file = models.FileField(upload_to=f'files')
--- Chunkr-Backend/chunkrio/urls.py
+++ Chunkr-Backend/chunkrio/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_view
 
-app_name = "chunkrio"
+# app_name = "chunkrio"
 
 urlpatterns = [
-    path('', views.home, name='chunkrio-home'),
-    path('documentation/', views.documentation, name='chunkr-documentation'),
-    path('userprofile/', views.userprofile, name='chunkr-userprofile'),
+    path('', views.home, name='home'),
+    path('documentation/', views.documentation, name='documentation'),
+    path('userprofile/', views.userProfile, name='userprofile'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_view.LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name="logout"),
 ]
