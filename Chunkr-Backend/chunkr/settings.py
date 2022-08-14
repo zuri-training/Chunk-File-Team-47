7ed168262dc313c37e384515ed99fc4c1f7d3fb2
Aluko Ayomide  Stephen <64385390+AyomiCoder@users.noreply.github.com>
2022-08-14 03:20:05 -0700

    Update settings.py

--- Chunkr-Backend/chunkr/settings.py
+++ Chunkr-Backend/chunkr/settings.py
@@ -80,25 +80,25 @@ WSGI_APPLICATION = 'chunkr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
- DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
- }
+#  DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+#      }
+#  }
 
-#DATABASES = {
-#       'default': {
- #          'ENGINE': 'djongo',
-  #         'CLIENT': {
-   #             'name': 'chunkr_db',
-    #            'host': 'mongodb+srv://chunkr:admin@cluster0.vnzpp.mongodb.net/?retryWrites=true&w=majority',
-     #           'username': 'chunkr',
-      #          'password': 'admin',
-       #         "authMechanism": 'SCRAM-SHA-1',
-        #    },
-       #}
-   #}
+DATABASES = {
+      'default': {
+          'ENGINE': 'djongo',
+          'CLIENT': {
+               'name': 'chunkr_db',
+               'host': 'mongodb+srv://chunkr:admin@cluster0.vnzpp.mongodb.net/?retryWrites=true&w=majority',
+               'username': 'chunkr',
+               'password': 'admin',
+               "authMechanism": 'SCRAM-SHA-1',
+           },
+       }
+   }
 
 
 # Password validation
